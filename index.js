149c45c79ee68e2c222da6b87db1d6e24b5f9ec5
Arthur <65594296+InTheDarkAlone@users.noreply.github.com>
2021-04-20 19:18:31 +0500

    1-5

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let r = new RegExp("\d+");
 let count = 2;
 
-let form = document.querySelector(".form-drink");
+let form = document.querySelector("form")
+let formDrink = document.querySelector(".form-drink");
 let bodyForm = document.querySelector(".forms-drinks");
 let addFormDrink = document.querySelector(".add-button");
 let sumbitButton = document.querySelector(".submit-button");
 
-addFormDrink.onclick = (e) => {
-    let copyForm = form.cloneNode(true);
+addFormDrink.addEventListener("click", (e) => {
+    let copyForm = formDrink.cloneNode(true);
     let h4 = copyForm.querySelector("h4");
     h4.textContent = h4.textContent.slice(0, -1) + count;
     bodyForm.append(copyForm);
@@ -15,19 +16,58 @@ addFormDrink.onclick = (e) => {
 
     let deleteFormDrink = document.createElement("button");
     deleteFormDrink.textContent = "Удалить ?"
-    deleteFormDrink.onclick = (e) => {
+    deleteFormDrink.addEventListener("click", (e) => {
         copyForm.remove();
         count--;
-    };
+    });
     copyForm.querySelector(".remove-element").append(deleteFormDrink);
-};
+})
 
-sumbitButton.onclick = (e) => {
-    e.preventDefault(); // убрать потом или как-нибудь обойти, короче тут фигня
-    let window = document.createElement("div");
-    window.textContent = "Ваш заказ оформлен"
-    window.classList.add("acceptance-window");
-    document.body.append(window);
+sumbitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let data = getData();
+
+    console.log(data);
+    let fon = document.createElement("div");
+    let dialogWindow = document.createElement("div");
+    let closeButton = document.createElement("button");
+
+    closeButton.textContent = "закрыть";
+    closeButton.classList.add("close-button");
+    closeButton.addEventListener("click", (e) => {
+        fon.remove();
+    });
+
+    fon.append(dialogWindow);
+    fon.classList.add("acceptance");
+
+    dialogWindow.textContent = `Ваш заказ оформлен Вы заказали ${data.count} drink(s)`;
+    dialogWindow.classList.add("acceptance-window");
+    dialogWindow.append(closeButton);
+
+    document.body.append(fon);
+});
+
+function getData() {
+    let orders = form.querySelectorAll('fieldset');
+    let result = {
+        count: orders.length,
+        drinks: []
+    };
+
+    for (let order of orders) {
+        let additionally = "";
+        for (let i of order.querySelectorAll('[name="options"]'))
+            if (i.checked)
+                additionally += i.value + ", ";
+        result.drinks.push({
+            drink: order.querySelector('[name="drink"]').value,
+            milk: order.querySelector('[name="milk"]').value,
+            additionally: additionally
+        })
+    }
+
+    return result;
 }
 
 
