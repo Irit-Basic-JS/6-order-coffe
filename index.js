23e062e182305c26b7cd864bd3dde61a4d9fb18e
bozhkovkost <71607999+bozhkovkost@users.noreply.github.com>
2021-06-08 00:55:13 +0500

    index.js (2 задание)

--- index.js
+++ index.js
@@ -6,35 +6,27 @@ const submitButton = document.querySelector('.submit-button');
 const overlay = document.querySelector('.overlay');
 const order = document.querySelector('.order');
 let beverageCount = 0;
-
-
 addButton.addEventListener('click', function () {
     const beverage = createBeverage(++beverageCount);
     document.querySelector('template').before(beverage);
 });
-
 form.addEventListener('click', function () {
     updateButtonsStatus();
 });
-
 submitButton.addEventListener('click', function (evt) {
     evt.preventDefault();
     overlay.style.display = 'block';
-
     createTableOrder();
     console.log(getData());
 });
-
 closeButton.addEventListener('click', function () {
     overlay.style.display = 'none';
 });
-
 window.addEventListener('click', function (evt) {
     if (evt.target === overlay) {
         overlay.style.display = 'none';
     }
 });
-
 function createBeverage(number) {
     const beverage = beverageTemplate.cloneNode(true);
     const title = beverage.querySelector('h4');
@@ -42,17 +34,16 @@ function createBeverage(number) {
 
     setMilkNames(beverage, number);
     addRemoveButton(beverage);
+    addWishes(beverage);
 
     return beverage;
 }
-
 function setMilkNames(beverage, number) {
     const milk = beverage.querySelectorAll('input[type="radio"]');
     for (const m of milk) {
         m.name = `milk-${number}`;
     }
 }
-
 function addRemoveButton(beverage) {
     const removeButton = beverage.querySelector('.remove-button');
     removeButton.onclick = function () {
@@ -61,7 +52,6 @@ function addRemoveButton(beverage) {
         updateOrder();
     }
 }
-
 function updateButtonsStatus() {
     const buttons = document.querySelectorAll('.remove-button');
     if (beverageCount === 1) {
@@ -72,7 +62,6 @@ function updateButtonsStatus() {
         }
     }
 }
-
 function updateOrder() {
     const beverages = document.querySelectorAll('.beverage');
     for (let i = 0; i < beverageCount; i++) {
@@ -82,7 +71,6 @@ function updateOrder() {
         setMilkNames(beverages[i], number);
     }
 }
-
 function getDeclination(count) {
     if (beverageCount % 100 > 9 && beverageCount % 100 < 21) {
         return 'напитков';
@@ -96,17 +84,14 @@ function getDeclination(count) {
         }
     }
 }
-
 function getData() {
     const beverages = document.querySelectorAll('.beverage');
     const result = [];
     for (const beverage of beverages) {
         const selectedBeverage = beverage.querySelector('select');
         const beverageName = selectedBeverage[selectedBeverage.selectedIndex].text;
-
         const selectedMilk = beverage.querySelector('input[type="radio"]:checked');
         const milkName = selectedMilk.parentElement.querySelector('span').textContent;
-
         const selectedOptions = beverage.querySelectorAll('input[type="checkbox"]:checked')
         const optionsName = [];
         for (const option of selectedOptions) {
@@ -114,44 +99,69 @@ function getData() {
             optionsName.push(name);
         }
 
+        const wishes = beverage.querySelector('textarea').value;
 
         result.push([
             beverageName,
             milkName,
             optionsName.join(', '),
+            wishes,
         ]);
     }
 
     return result;
 }
-
 function createTableOrder() {
     order.innerHTML = '';
-
     const caption = document.createElement('caption');
     const declination = getDeclination(beverageCount);
     caption.textContent = `Вы заказали: ${beverageCount} ${declination}`;
     order.append(caption);
 
     const tableHeader = document.createElement('tr');
-    const headers = ['Напиток', 'Молоко', 'Дополнительно']
+    const headers = ['Напиток', 'Молоко', 'Дополнительно', 'Пожелания']
     for (const header of headers) {
         const th = document.createElement('th');
         th.append(header);
         tableHeader.append(th);
     }
     order.append(tableHeader)
-
     const beverages = getData();
     for (const beverage of beverages) {
         const tr = document.createElement('tr');
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < headers.length; i++) {
             const td = document.createElement('td');
             td.append(beverage[i] || '-');
             tr.append(td);
         }
         order.append(tr);
     }
+}
+
+function addWishes(beverage) {
+    const wishes = beverage.querySelector('textarea');
+    wishes.oninput = function () {
+        const content = beverage.querySelector('.content');
+        content.innerHTML = highlightImportant(wishes.value);
+    };
+}
+
+function highlightImportant(text) {
+    const importantWords = [
+        'срочно', 'побыстрее', 'быстрее',
+        'поскорее', 'скорее', 'очень нужно',
+    ];
+
+    for (const word of importantWords) {
+        const matchesArray = text.match(new RegExp(`${word}(?!</b>)`, 'gi'));
+        if (matchesArray) {
+            for (const match of matchesArray) {
+                text = text.replaceAll(match, `<b>${match}</b>`);
+            }
+        }
+    }
+
+    return text;
 }
 
 addButton.click();
