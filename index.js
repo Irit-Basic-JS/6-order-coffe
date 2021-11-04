f4257c3515b3bd536710cf80088ba2674da7bd4d
Tripletri <Tripletri@yandex.ru>
2021-11-04 16:09:35 +0500

    DrinkForm now adds drink fields.

--- index.js
+++ index.js
@@ -1,72 +1,25 @@
 class DrinkForm {
-    #drinkFields = `
-    <fieldset class="beverage">
-      <h4 class="beverage-count">Напиток №1</h4>
-      <label class="field">
-        <span class="label-text">Я буду</span>
-        <select>
-          <option value="espresso">Эспрессо</option>
-          <option value="capuccino" selected>Капучино</option>
-          <option value="cacao">Какао</option>
-        </select>
-      </label>
-      <div class="field">
-        <span class="checkbox-label">Сделайте напиток на</span>
-        <label class="checkbox-field">
-          <input type="radio" name="milk" value="usual" checked />
-          <span>обычном молоке</span>
-        </label>
-        <label class="checkbox-field">
-          <input type="radio" name="milk" value="no-fat" />
-          <span>обезжиренном молоке</span>
-        </label>
-        <label class="checkbox-field">
-          <input type="radio" name="milk" value="soy" />
-          <span>соевом молоке</span>
-        </label>
-        <label class="checkbox-field">
-          <input type="radio" name="milk" value="coconut" />
-          <span>кокосовом молоке</span>
-        </label>
-      </div>
-      <div class="field">
-        <span class="checkbox-label">Добавьте к напитку:</span>
-        <label class="checkbox-field">
-          <input type="checkbox" name="options" value="whipped cream" />
-          <span>взбитых сливок</span>
-        </label>
-        <label class="checkbox-field">
-          <input type="checkbox" name="options" value="marshmallow" />
-          <span>зефирок</span>
-        </label>
-        <label class="checkbox-field">
-          <input type="checkbox" name="options" value="chocolate" />
-          <span>шоколад</span>
-        </label>
-        <label class="checkbox-field">
-          <input type="checkbox" name="options" value="cinnamon" />
-          <span>корицу</span>
-        </label>
-      </div>
-    </fieldset>
-    `;
-    #drinksCount = 0;
+    #drinksCount = 1;
 
     constructor() {
         this.form = document.createElement('form');
-        this.addButton = DrinkForm.#createAddButton();
+        this.addButton = this.#createAddButton();
         this.submitButton = DrinkForm.#createSubmitButton();
 
         this.form.appendChild(this.addButton);
         this.form.appendChild(this.submitButton);
     }
 
-    static #createAddButton() {
-        const addButton = document.createElement('button');
-        addButton.type = 'button';
-        addButton.classList.add('add-button');
-        addButton.innerHTML = '+ Добавить напиток';
-        return addButton;
+    appendForm(element) {
+        element.appendChild(this.form);
+    }
+
+    addDrinkFields() {
+        const div = document.createElement('div');
+        div.innerHTML = this.#createDrinkFields();
+
+        this.form.insertBefore(div, this.addButton);
+        this.#drinksCount++;
     }
 
     static #createSubmitButton() {
@@ -79,15 +32,70 @@ class DrinkForm {
         return submitButton;
     }
 
-    appendForm(element) {
-        element.appendChild(this.form);
+    #createDrinkFields() {
+        return `
+    <fieldset class="beverage">
+      <h4 class="beverage-count">Напиток №${this.#drinksCount}</h4>
+      <label class="field">
+        <span class="label-text">Я буду</span>
+        <select name="drink-name_${this.#drinksCount}">
+          <option value="espresso">Эспрессо</option>
+          <option value="capuccino" selected>Капучино</option>
+          <option value="cacao">Какао</option>
+        </select>
+      </label>
+      <div class="field">
+        <span class="checkbox-label">Сделайте напиток на</span>
+        <label class="checkbox-field">
+          <input type="radio" name="milk_${this.#drinksCount}" value="usual" checked />
+          <span>обычном молоке</span>
+        </label>
+        <label class="checkbox-field">
+          <input type="radio" name="milk_${this.#drinksCount}" value="no-fat" />
+          <span>обезжиренном молоке</span>
+        </label>
+        <label class="checkbox-field">
+          <input type="radio" name="milk_${this.#drinksCount}" value="soy" />
+          <span>соевом молоке</span>
+        </label>
+        <label class="checkbox-field">
+          <input type="radio" name="milk_${this.#drinksCount}" value="coconut" />
+          <span>кокосовом молоке</span>
+        </label>
+      </div>
+      <div class="field">
+        <span class="checkbox-label">Добавьте к напитку:</span>
+        <label class="checkbox-field">
+          <input type="checkbox" name="options_${this.#drinksCount}" value="whipped cream" />
+          <span>взбитых сливок</span>
+        </label>
+        <label class="checkbox-field">
+          <input type="checkbox" name="options_${this.#drinksCount}" value="marshmallow" />
+          <span>зефирок</span>
+        </label>
+        <label class="checkbox-field">
+          <input type="checkbox" name="options_${this.#drinksCount}" value="chocolate" />
+          <span>шоколад</span>
+        </label>
+        <label class="checkbox-field">
+          <input type="checkbox" name="options_${this.#drinksCount}" value="cinnamon" />
+          <span>корицу</span>
+        </label>
+      </div>
+    </fieldset>
+    `;
     }
 
-    addDrinkFields() {
-        const div = document.createElement('div');
-        div.innerHTML = this.#drinkFields;
+    #createAddButton() {
+        const addButton = document.createElement('button');
+        addButton.type = 'button';
+        addButton.classList.add('add-button');
+        addButton.innerHTML = '+ Добавить напиток';
+        addButton.onclick = () => {
+            this.addDrinkFields();
+        };
 
-        this.form.insertBefore(div, this.addButton);
+        return addButton;
     }
 
 }
@@ -96,6 +104,5 @@ class DrinkForm {
     const body = document.getElementsByTagName('body')[0];
     const form = new DrinkForm();
     form.appendForm(body);
-    form.addDrinkFields();
     form.addDrinkFields();
 })();
