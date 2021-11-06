d0b98f70419574c688db4a3e8879afce7de8c790
Tripletri <Tripletri@yandex.ru>
2021-11-06 16:30:10 +0500

    All tasks done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function declOfNum(number, words) {
+    return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(
+        number) % 10 : 5]];
+}
+
+function highlightWords(text) {
+    const regex = /срочно|быстрее|побыстрее|скорее|поскорее|очень нужно/gmi;
+    return text.replace(regex, (a, b) => {
+        return "<b>" + a + "</b>";
+    });
+}
+
 class DrinkFieldset {
     numberHandler;
     onDelete;
@@ -9,14 +21,12 @@ class DrinkFieldset {
     createElement() {
         const fieldset = this.#createFieldset();
         const [header, changeNumber] = this.#createHeader(fieldset);
-        const drinkTypeField = this.#createDrinkTypeField();
-        const milkField = this.#createMilkField();
-        const optionsField = this.#createOptionsField();
 
         fieldset.appendChild(header);
-        fieldset.appendChild(drinkTypeField);
-        fieldset.appendChild(milkField);
-        fieldset.appendChild(optionsField);
+        fieldset.appendChild(this.#createDrinkTypeField());
+        fieldset.appendChild(this.#createMilkField());
+        fieldset.appendChild(this.#createOptionsField());
+        fieldset.appendChild(this.#createWishesField());
 
         return [fieldset, changeNumber];
     }
@@ -37,7 +47,7 @@ class DrinkFieldset {
         changer();
 
         const closeButton = document.createElement("span");
-        closeButton.style = "margin-left: auto;";
+        closeButton.classList.add('btn-close');
         closeButton.innerHTML = "X";
         closeButton.onclick = () => {
             const event = {
@@ -123,10 +133,30 @@ class DrinkFieldset {
         `;
         return optionsField;
     }
+
+    #createWishesField() {
+        const container = document.createElement('div');
+        container.classList.add('feedback-container');
+        const textarea = document.createElement('textarea');
+        const output = document.createElement('div');
+
+        textarea.classList.add('feedback');
+        textarea.name = 'feedback';
+        textarea.oninput = () => {
+            output.innerHTML = highlightWords(textarea.value);
+        };
+
+        output.classList.add('feedback');
+        container.appendChild(textarea);
+        container.appendChild(output);
+
+        return container;
+    }
+
 }
 
 class DrinkFormsManager {
-    #drinksCount = 0;
+    drinksCount = 0;
     #container;
     #addButton;
     #drinkForms = [];
@@ -137,7 +167,7 @@ class DrinkFormsManager {
     }
 
     createElement() {
-        const submitButton = DrinkFormsManager.#createSubmitButton();
+        const submitButton = this.#createSubmitButton();
         this.#container.appendChild(this.#addButton);
         this.#container.appendChild(submitButton);
 
@@ -145,36 +175,45 @@ class DrinkFormsManager {
     }
 
     appendForm() {
-        this.#drinksCount++;
+        this.drinksCount++;
 
-        const form = new DrinkForm(this.#drinksCount);
+        const form = new DrinkForm(this.drinksCount);
         const [element, drinkFieldset, changeNumber] = form.createElement();
 
         drinkFieldset.onDelete = (event, id) => {
-            if (this.#drinksCount === 1)
+            if (this.drinksCount === 1)
                 event.prevent();
             else {
-                this.#drinksCount--;
+                this.drinksCount--;
+                let i = 0;
+                let formToDelete;
                 for (const [form, updateNumber] of this.#drinkForms) {
+                    if (form.number === id)
+                        formToDelete = i;
                     if (form.number > id) {
                         form.number--;
                         updateNumber();
                     }
+                    i++;
                 }
+                this.#drinkForms.splice(formToDelete, 1);
             }
-
-
         };
         this.#drinkForms.push([form, changeNumber]);
         this.#container.insertBefore(element, this.#addButton);
     }
 
-    static #createSubmitButton() {
+    #createSubmitButton() {
         const submitButton = document.createElement('button');
         submitButton.classList.add('submit-button');
         submitButton.type = 'submit';
         submitButton.style = "margin-top: 30px";
         submitButton.innerHTML = 'Готово';
+
+        const modal = new ModalWindow(this);
+        submitButton.onclick = () => {
+            modal.show();
+        };
 
         return submitButton;
     }
@@ -190,21 +229,150 @@ class DrinkFormsManager {
 
         return addButton;
     }
+
+    getDrinks() {
+        return this.#drinkForms.map(value => value[0]);
+
+    }
 }
 
 class DrinkForm {
     number;
+    form;
 
     constructor(number) {
         this.number = number;
+        this.form = document.createElement('form');
     }
 
     createElement() {
         const drinkFieldset = new DrinkFieldset(() => this.number);
         const [element, changeNumber] = drinkFieldset.createElement();
+        this.form.appendChild(element);
+        return [this.form, drinkFieldset, changeNumber];
+    }
+
+    getDrink() {
+        const formData = new FormData(this.form);
+        const drink = new Drink();
+        drink.name = this.#translateValue(formData.get('drink-name'));
+        drink.milk = this.#translateValue(formData.get('milk'));
+        drink.options = formData.getAll('options').map(value => this.#translateValue(value));
+        drink.wishes = highlightWords(formData.get('feedback'));
+        return drink;
+    }
+
+    #translateValue(value) {
+        const dictionary = {
+            "espresso": "Эспрессо",
+            "cappuccino": "Капучино",
+            "cacao": "Какао",
+            "usual": "Обычное молоко",
+            "no-fat": "Обезжиренное молоко",
+            "soy": "Соевое молоко",
+            "coconut": "Кокосовое молоко",
+            "whipped cream": "взбитые сливки",
+            "marshmallow": "зефирки",
+            "chocolate": "шоколад",
+            "cinnamon": "корица"
+        };
+        return dictionary[value];
+    }
+}
+
+class Drink {
+    name;
+    milk;
+    options = [];
+    wishes;
+}
+
+class ModalWindow {
+    modalWindow;
+    content;
+    drinkManager;
+    drinksCountHeader;
+
+    constructor(drinkManager) {
+        this.drinkManager = drinkManager;
+        this.modalWindow = document.getElementById('modal');
+        this.drinksCountHeader = document.createElement("h3");
+        this.content = document.getElementById("modal-content");
+        this.content.appendChild(this.drinksCountHeader);
+
+        document.getElementById('modal-close').onclick = () => {
+            this.hide();
+        };
+
+        const label = document.createElement('label');
+        label.innerHTML = 'Выберите время заказа';
+
+        const date = document.createElement('input');
+        date.type = 'date';
+        date.name = 'order-date';
+        date.required = true;
+
+        const submit = document.createElement('button');
+        submit.type = 'submit';
+        submit.classList.add("submit-button");
+        submit.innerHTML = 'Оформить';
+
         const form = document.createElement('form');
-        form.appendChild(element);
-        return [form, drinkFieldset, changeNumber];
+        form.appendChild(label);
+        form.appendChild(date);
+        form.appendChild(submit);
+        form.onsubmit = (ev) => {
+            ev.preventDefault();
+            const data = new FormData(form);
+            const date = Date.parse(data.get('order-date'));
+            const today = new Date().setHours(0, 0, 0, 0);
+            if (date >= today) {
+                this.hide();
+            } else {
+                alert("Мы не умеем перемещаться во времени. Выберите время позже, чем текущее");
+            }
+        };
+
+        const footer = document.getElementById('modal-footer');
+        footer.appendChild(form);
+    }
+
+    show() {
+        this.modalWindow.style.visibility = 'visible';
+        this.drinksCountHeader.innerHTML = `Вы заказали ${this.drinkManager.drinksCount}
+         ${declOfNum(this.drinkManager.drinksCount,
+            ['напиток', 'напитка', 'напитков'])}!`;
+
+        this.#addDrinksToTable(this.drinkManager.getDrinks());
+    }
+
+    #addDrinksToTable(drinks) {
+        const table = document.getElementById('table-summary');
+        document.querySelectorAll("#summary-row").forEach(row => row.remove());
+        let i = 1;
+        for (const form of drinks) {
+            const drink = form.getDrink();
+            const row = table.insertRow(i);
+
+            const nameCell = row.insertCell(0);
+            nameCell.innerHTML = drink.name;
+
+            const milkCell = row.insertCell(1);
+            milkCell.innerHTML = drink.milk;
+
+            const optionsCell = row.insertCell(2);
+            optionsCell.innerHTML = drink.options.join(', ');
+
+            const wishesCell = row.insertCell(3);
+            wishesCell.innerHTML = drink.wishes;
+
+            row.id = "summary-row";
+            i++;
+        }
+    }
+
+    hide() {
+        this.modalWindow.style.visibility = 'collapse';
     }
 }
 
